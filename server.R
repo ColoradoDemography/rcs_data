3b7518df30174dc53b2a5445053f2cccf13dee87
robkemp <rjameskemp@gmail.com>
2016-12-05 14:16:25 -0700

    remove row names

--- server.R
+++ server.R
@@ -10,35 +10,40 @@ function(input, output, session) {
   output$contact=renderTable({
     contact%>%
       filter(id==id())%>%
-      gather(Field, Current)}
+      gather(Field, Current)}, 
+    include.rownames=FALSE
   )
   
   output$permits=renderTable({ 
     bp%>%
       filter(id==id())%>%
       select(year:dec)},
-    digits=0
+    digits=0, 
+    include.rownames=FALSE
     )
   
   output$co=renderTable({
     co%>%
       filter(id==id())%>%
       select(year:dec)},
-    digits=0
+    digits=0, 
+    include.rownames=FALSE
   )
 
   output$demo=renderTable({
     demo%>%
       filter(id==id())%>%
       select(year:dec)},
-    digits=0
+    digits=0, 
+    include.rownames=FALSE
   )
 
   output$mobile=renderTable({
     mobile%>%
       filter(id==id())%>%
       select(-id:-place)},
-    digits=0
+    digits=0, 
+    include.rownames=FALSE
   )
   
   output$sdo=renderTable({
@@ -48,7 +53,8 @@ function(input, output, session) {
              HousingUnitChange=HousingUnits-lag(HousingUnits))%>%
       select(year, Population, HousingUnits, PopulationChange, HousingUnitChange)%>%
       filter(year<2015)},
-                         digits = 0)
+                         digits = 0, 
+    include.rownames=FALSE)
 
 output$census=renderTable({
   census%>%
@@ -57,13 +63,15 @@ output$census=renderTable({
            HousingUnitChange=HousingUnits-lag(HousingUnits))%>%
     select(year, Population, HousingUnits, PopulationChange, HousingUnitChange)%>%
     filter(year<2015)},
-  digits = 0)
+  digits = 0, 
+  include.rownames=FALSE)
 
 output$annex=renderTable({
   annex%>%
     filter(id==id())%>%
     select(year, PopulationChange:GroupQuartersChange)%>%
     filter(year<2015)},
-  digits = 0)
+  digits = 0, 
+  include.rownames=FALSE)
 
 }

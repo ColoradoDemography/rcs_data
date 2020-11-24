6bc8dc3d6fcc56d1f2a27af2e9d3039465d450d9
ABSOD <32849428+ABSOD@users.noreply.github.com>
2020-11-24 16:53:05 -0700

    Updated for V20

--- server.R
+++ server.R
@@ -3,75 +3,47 @@ library(plotly)
 source("setup.R")
 
 function(input, output, session) {
-
-  id=reactive({as.numeric(filter(areas, Name==input$area)%>%
-                              select(id))})
+  observeEvent(input$area, {
+ 
+    plNm <- reactive(input$area)
+    plName <- as.character(isolate(plNm()))
+    
+  idSel = areas %>% filter( Name== plName ) %>%  select(id)
   
-  output$contact=renderTable({
-    contact%>%
-      filter(id==id())%>%
-      gather(Field, Current)}, 
-    include.rownames=FALSE
-  )
   
-  output$permits=renderTable({ 
+  
+   output$permits=renderTable({ 
     bp%>%
-      filter(id==id())%>%
-      select(year:dec)},
+      filter(id==idSel$id)%>%
+      select(year:December)},
     digits=0, 
     include.rownames=FALSE
     )
   
   output$co=renderTable({
     co%>%
-      filter(id==id())%>%
-      select(year:dec)},
+      filter(id==idSel$id)%>%
+      select(year:December)},
     digits=0, 
     include.rownames=FALSE
   )
 
   output$demo=renderTable({
     demo%>%
-      filter(id==id())%>%
-      select(year:dec)},
+      filter(id==idSel$id)%>%
+      select(year:December)},
     digits=0, 
     include.rownames=FALSE
   )
 
   output$mobile=renderTable({
     mobile%>%
-      filter(id==id())%>%
-      select(-id:-place)},
+      filter(id==idSel$id)%>%
+      select(year, JantoJun, JultoDec)},
     digits=0, 
     include.rownames=FALSE
   )
-  
-  output$sdo=renderTable({
-    sdo%>%
-      filter(id==id())%>%
-      mutate(PopulationChange=Population-lag(Population),
-             HousingUnitChange=HousingUnits-lag(HousingUnits))%>%
-      select(year, Population, HousingUnits, PopulationChange, HousingUnitChange)%>%
-      filter(year<2019)},
-                         digits = 0, 
-    include.rownames=FALSE)
+ 
 
-output$census=renderTable({
-  census%>%
-    filter(id==id())%>%
-    mutate(PopulationChange=Population-lag(Population),
-           HousingUnitChange=HousingUnits-lag(HousingUnits))%>%
-    select(year, Population, HousingUnits, PopulationChange, HousingUnitChange)%>%
-    filter(year<2019)},
-  digits = 0, 
-  include.rownames=FALSE)
-
-output$annex=renderTable({
-  annex%>%
-    filter(id==id())%>%
-    select(year, PopulationChange:GroupQuartersChange)%>%
-    filter(year<2019)},
-  digits = 0, 
-  include.rownames=FALSE)
-
+})
 }
--- ui.R
+++ ui.R
@@ -1,3 +1,5 @@
+# Revision of RCS_Data application V2020 A. Bickford November 2020
+
 library(plotly)
 library(shiny)
 
@@ -6,14 +8,10 @@ source("setup.R")
 function(req) {
   htmlTemplate("index.html",
                area=selectInput("area","Select your Area:", choices = unique(areas$Name)),               
-               contact_info=tableOutput("contact"),
                permits=tableOutput("permits"),
                co=tableOutput("co"),
                demo=tableOutput("demo"),
-               mobile=tableOutput("mobile"),
-               sdo_estimates=tableOutput("sdo"),
-               census_estimates=tableOutput("census"),
-               annexations=tableOutput("annex")
+               mobile=tableOutput("mobile")
                )
 }
 
